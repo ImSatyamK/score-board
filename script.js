bf98103a05c84cef47f2f7db53c883a5863ebbb2
ImSatyamK <189542961+ImSatyamK@users.noreply.github.com>
2025-10-28 10:16:12 +0530

    add team name edit feature

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
 let t1score = 0;
 let t2score = 0;
 
+const team1scoreEl = document.getElementById('team1score')
+const team2scoreEl = document.getElementById('team2score')
+
+const team1nameBoxEl = document.getElementById('team1name-box')
+const team2nameBoxEl = document.getElementById('team2name-box')
+
+const team1inputBoxEl = document.getElementById('team1input-box')
+const team2inputBoxEl = document.getElementById('team2input-box')
+
 function addScore(score, team){
     if (team === 'team1score'){
         t1score += score;
-        document.getElementById(team).textContent = t1score;
+        team1scoreEl.textContent = t1score;
         return;
     }
     t2score += score;
-    document.getElementById(team).textContent = t2score;
+    team2scoreEl.textContent = t2score;
 }
 
 function resetScore(){
     t1score = 0;
     t2score = 0;
-    document.getElementById('team1score').textContent = t1score;
-    document.getElementById('team2score').textContent = t2score;
+    team1scoreEl.textContent = t1score;
+    team2scoreEl.textContent = t2score;
+}
+
+function editTeam(team){
+    if (team === 'team1'){
+        team1nameBoxEl.style.display = 'none';
+        team1inputBoxEl.style.display = 'flex';
+        return;
+    }
+    team2nameBoxEl.style.display = 'none';
+    team2inputBoxEl.style.display = 'flex';
+}
+
+function saveTeam(teamNumber) {
+    let tName = document.getElementById(`team${teamNumber}name-input`).value;
+    document.getElementById(`team${teamNumber}name`).textContent = tName || `TEAM ${teamNumber}`;
+    if (teamNumber === 1){
+        team1nameBoxEl.style.display = 'flex';
+        team1inputBoxEl.style.display = 'none';
+        return;
+    }
+    team2nameBoxEl.style.display = 'flex';
+    team2inputBoxEl.style.display = 'none';
 }
